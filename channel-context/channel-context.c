8168998f40f377ede3806f2b258b523cbbf5421f
Valerie Pond <79415174+ValwareIRC@users.noreply.github.com>
2022-04-27 10:13:10 +0100

    Update channel-context.c

--- channel-context/channel-context.c
+++ channel-context/channel-context.c
@@ -3,6 +3,10 @@
   Copyright Ⓒ 2022 Valerie Pond
   Channel Context
 
+  Spec authors:
+  delthas
+  delthas@dille.cc
+
   Sends a message tag with the channel for which the message itself is associated with
   i.e. if someone types in a channel "!help", the bot will reply in private to the user
   but with the channel sent as a mtag, so that the user may know which channel the response
