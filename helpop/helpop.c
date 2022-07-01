da9e5ab59027ee16c3faa1d0960f1310b751c7e0
Valerie Pond <79415174+ValwareIRC@users.noreply.github.com>
2022-07-01 22:28:54 +0100

    Update helpop.c

--- helpop/helpop.c
+++ helpop/helpop.c
@@ -2,15 +2,10 @@
   Licence: GPLv3
   Copyright Ⓒ 2022 Valerie Pond
   
-  Provides "helpop" by Valware :D
+  Helpop
   usermode h (helpop) (settable by IRCops only)
   channelmode g (helpop-only)
-  command HELPOP <string to send to other helpops>
-  command REPORT <string for users to send to helpops>
-  
-  parts taken from unrealircd source code, and Gottem's modules and dank module templates ayyyy
-  shout out to Syzop for the epic documentation of unrealircd module api :D
-  my first module submission lmao
+  command HELPOPS <string to send to other helpops>
   
   this module have no configurable option
 */
@@ -19,13 +14,8 @@ module
 {
         documentation "https://github.com/ValwareIRC/valware-unrealircd-mods/blob/main/helpop/README.md";
 	troubleshooting "In case of problems, documentation or e-mail me at v.a.pond@outlook.com";
-        min-unrealircd-version "5.*";
-        // THE FOLLOWING FIELDS ARE OPTIONAL:
-        // Maximum version that this module supports:
-        max-unrealircd-version "5.*";
-        // This text is displayed after running './unrealircd module install ...'
-        // It is recommended not to make this an insane number of lines and refer to a URL instead
-        // if you have lots of text/information to share:
+        min-unrealircd-version "6.*";
+        max-unrealircd-version "6.*";
         post-install-text {
                 "The module is installed. Now all you need to do is add a loadmodule line:";
                 "loadmodule \"third/helpop\";";
@@ -38,16 +28,14 @@ module
 #include "unrealircd.h"
 
 Cmode_t EXTCMODE_HELPOP;
-
+long extumode_helpop = 0L;
 #define HELPONLY_FLAG 'g'
 #define UMODE_HELPOP 'h' 
-#define IsHelpOnly(channel)    (channel->mode.extmode & EXTCMODE_HELPOP)
+#define IsHelpOnly(channel)    (channel->mode.mode & EXTCMODE_HELPOP)
 #define IsHelpop(x) (IsUser(x) && (x)->umodes & extumode_helpop)
-#define WHOIS_HELPOP_STRING ":%s 313 %s %s :is available for help"
-#define MSG_MYCMD "HELPOPER"
-#define REP_MYCMD "REPORT"
+#define MSG_MYCMD "HELPOPS"
 
-CMD_FUNC(HELPOPER); // Register command function
+CMD_FUNC(HELPOPS); // Register command function
 CMD_FUNC(REPORT);
 
 #define CheckAPIError(apistr, apiobj) \
@@ -58,25 +46,20 @@ CMD_FUNC(REPORT);
 		} \
 	} while(0)
 
-long extumode_helpop = 0L;
 
 
 ModuleHeader MOD_HEADER = {
 	"third/helpop", // Module name
-	"1.0", // Module Version
-	"HelpOp", // Description
+	"1.4", // Module Version
+	"HelpOp - Provides usermode h (HelpOp) and swhois line, channelmode g (HelpOp-only room), and command /HELPOPS", // Description
 	"Valware", // Author
-	"unrealircd-5", // Unreal Version
+	"unrealircd-6", // Unreal Version
 };
 
-int helpop_whois(Client *requester, Client *acptr);
-int helponly_check (Client *client, Channel *channel, char *key, char *parv[]);
+int helpop_whois(Client *requester, Client *acptr, NameValuePrioList **list);
+int helponly_check (Client *client, Channel *channel, const char *key, char **errmsg);
+int helpchan_join_op_presence_check(Client *client, Channel *channel, MessageTag *mtags);
 
-typedef struct {
-	// Change this or add more variables, whatever suits you fam
-	char flag;
-	int p;
-} aModeX;
 
 // Initialisation routine (register hooks, commands and modes or create structs etc)
 MOD_INIT() {
@@ -84,16 +67,16 @@ MOD_INIT() {
 
 	memset(&req, 0, sizeof(req));
 	req.paracount = 0;
-	req.flag = HELPONLY_FLAG;
+	req.letter = HELPONLY_FLAG;
 	req.is_ok = extcmode_default_requirehalfop;
 	CmodeAdd(modinfo->handle, req, &EXTCMODE_HELPOP);
 	HookAdd(modinfo->handle, HOOKTYPE_CAN_JOIN, 0, helponly_check);
 
 	CheckAPIError("UmodeAdd(extumode_helpop)", UmodeAdd(modinfo->handle, UMODE_HELPOP, UMODE_GLOBAL, 0, umode_allow_opers, &extumode_helpop));
-	CheckAPIError("CommandAdd(MSG_MYCMD)", CommandAdd(modinfo->handle, MSG_MYCMD, HELPOPER, 1, CMD_SERVER | CMD_USER));
-	CheckAPIError("CommandAdd(REP_MYCMD)", CommandAdd(modinfo->handle, REP_MYCMD, REPORT, 1, CMD_SERVER | CMD_USER));
+	CheckAPIError("CommandAdd(MSG_MYCMD)", CommandAdd(modinfo->handle, MSG_MYCMD, HELPOPS, 1, CMD_SERVER | CMD_USER));
 	
 	HookAdd(modinfo->handle, HOOKTYPE_WHOIS, 0, helpop_whois);
+	HookAdd(modinfo->handle, HOOKTYPE_LOCAL_JOIN, 0, helpchan_join_op_presence_check);
 
 	MARK_AS_GLOBAL_MODULE(modinfo);
 	return MOD_SUCCESS;
@@ -104,27 +87,31 @@ MOD_LOAD() {
 	return MOD_SUCCESS;
 }
 
-// Called on unload/rehash obv
 MOD_UNLOAD() {
 	return MOD_SUCCESS;
 }
-int helponly_check (Client *client, Channel *channel, char *key, char *parv[])
+int helponly_check (Client *client, Channel *channel, const char *key, char **errmsg)
 {
 	if (IsHelpOnly(channel) && !IsHelpop(client)) {
-		sendnotice(client, "*** (%s) You must be a logged in member of staff to join that room.",channel->chname);
-		return ERR_NEEDREGGEDNICK;
+		
+		*errmsg = ":This channel is for Helpops only (%s)", channel->name, channel->name;
+		return ERR_NOPRIVILEGES;
 	}
 	return 0;
 }
 
-int helpop_whois(Client *requester, Client *acptr)
+int helpop_whois(Client *requester, Client *acptr, NameValuePrioList **list)
 {
+	char buf[512];
 	int hideoper = (IsHideOper(acptr) && (requester != acptr) && !IsOper(requester)) ? 1 : 0;
 
-	if (IsHelpop(acptr) && !hideoper)
-		sendto_one(requester, NULL, WHOIS_HELPOP_STRING, me.name, requester->name, acptr->name);
-
+	if (hideoper)
+		return 0;
+	if (IsHelpop(acptr))
+		add_nvplist_numeric_fmt(list, 0, "helpop", acptr, 320, "%s :is available for help", acptr->name);
+	
 	return 0;
+	
 }
 
 static void dumpit(Client *client, char **p);
@@ -134,30 +121,14 @@ static char *helpop_help[] = {
 	** \002 = bold -- \x02
 	** \037 = underlined -- \x1F
 	*/
-	"*** \002Help on /HELPOPER\002 ***",
+	"*** \002Help on /HELPOPS\002 ***",
 	"DESC",
 	" ",
 	"Syntax:",
-	"    \002/HELPOPER\002 \037<msg>\037",
+	"    \002/HELPOPS\002 \037<msg>\037",
 	" ",
 	"Examples:",
-	"    \002/HELPOPER Help needed in #channel\002",
-	NULL
-};
-
-static char *report_help[] = {
-	/* Special characters:
-	** \002 = bold -- \x02
-	** \037 = underlined -- \x1F
-	*/
-	"*** \002Help on /REPORT\002 ***",
-	"DESC",
-	" ",
-	"Syntax:",
-	"    \002/REPORT\002 \037<msg>\037",
-	" ",
-	"Examples:",
-	"    \002/REPORT Guest1072 is being abusive in #friendly\002",
+	"    \002/HELPOPS Help needed in #channel\002",
 	NULL
 };
 
@@ -171,43 +142,57 @@ static void dumpit(Client *client, char **p) {
 		sendto_one(client, NULL, ":%s %03d %s :%s", me.name, RPL_TEXT, client->name, *p);
 
 	// Let user take 8 seconds to read it
-	client->local->since += 8;
+	add_fake_lag(client, 8000);
 }
 
-CMD_FUNC(REPORT) {
 
-	if (parc < 2 || BadPtr(parv[1])) {
-		dumpit(client, report_help);
-		return;
-	}
-	else if (IsHelpop(client)) {
-		sendnotice(client, "*** /REPORT is for non-helpops. Try /HELPOPER instead");
-		return;
-	}
-	else {
-		sendnotice(client, "*** Thank you. Your report has been submitted.");
-		sendto_umode_global(extumode_helpop,"*** REPORT by %s: %s",client->name,parv[1]);
-
-	}
-	return;
-
-}
-	
-
-CMD_FUNC(HELPOPER) {
+CMD_FUNC(HELPOPS) {
 
 	if(parc < 2 || BadPtr(parv[1])) { // If first argument is a bad pointer, don't proceed
 		dumpit(client, helpop_help); // Return help string instead
 		return;
 	}
-
-	else if (IsHelpop(client)) {
-		sendto_umode_global(extumode_helpop,"*** HelpOper: from %s: %s",client->name,parv[1]);
-	}
-	else {
-		sendnotice(client, "*** You may not use this command.");
+	else if (!IsHelpop(client)) {
+		sendnumeric(client, ERR_NOPRIVILEGES);
 		return;
 	}
-	return;
+	sendto_umode_global(extumode_helpop,"(HelpOps) from %s: %s",client->name,parv[1]);
 
+}
+
+int helpchan_join_op_presence_check(Client *client, Channel *channel, MessageTag *mtags)
+{
+	if (strcasecmp(channel->name,HELP_CHANNEL))
+		return 0;
+	
+	Member *member;
+	int found = 0;
+	for (member = channel->members; member; member = member->next)
+	{
+		Membership *mb = find_membership_link(member->client->user->channel, channel);
+		if (IsHelpop(member->client) && client == member->client)
+		{
+			MessageTag *mtags = NULL;
+	
+			new_message(member->client, NULL, &mtags);
+			sendto_channel(channel, &me, NULL, 0, 0, SEND_ALL, mtags,
+						":%s MODE %s %s %s",
+						me.name, channel->name, "+o", client->name);
+			sendto_server(NULL, 0, 0, mtags, ":%s MODE %s %s %s%s", me.id, channel->name, "+o", client->name, IsServer(member->client)?" 0":"");
+			free_message_tags(mtags);
+			add_member_mode_fast(member, mb, 'o');
+			found = 1;
+		}
+		if (check_channel_access(member->client, channel, "hoaq"))
+		{
+
+			if (!IsULine(member->client))
+				found = 1;
+		}		
+	}
+	if (!found) // found no op so let them helpops know
+	{
+		sendto_umode(extumode_helpop,"[%s] %s has joined the help channel and there are no ops on that channel.", HELP_CHANNEL, client->name);
+	}
+	return 0;
 }
